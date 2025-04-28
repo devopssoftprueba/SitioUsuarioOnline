a7c901558387aa63649a5ee31f041fa5eacedd68
Ronald-pelaez <ronaldpelaez201@gmail.com>
2025-04-28 11:15:19 -0500

    Update suma.ts

--- suma.ts
+++ suma.ts
@@ -1,13 +1,6 @@
 
 class ExampleClass {
-    /**
-     * Metodo que toma dos parámetros y devuelve un string con ambos valores.
-     *
-     * @param param1 - El primer parámetro de tipo string.
-     * @param param2 - El segundo parámetro de tipo número.
-     * @returns Un string que combina ambos parámetros.
-     * @description Este metodo toma dos parámetros y devuelve una cadena que los combina.
-     */
+ 
     exampleMethod(param1: string, param2: number): string {
         return `Param1: ${param1}, Param2: ${param2}`;
     }
