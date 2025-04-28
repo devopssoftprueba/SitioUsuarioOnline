4d8149dec1cab2ef63680f275b46f3a11b4185b8
Ronald Pelaez <ronald.jimenez@virtualsoft.tech>
2025-04-28 17:10:19 -0500

    suma.ts viene desde la rama con errores de documentacion le agrego un codigo sin errores

--- suma.ts
+++ suma.ts
@@ -1,8 +1,8 @@
 
 class ExampleClass {
  
-    exampleMethod(param1: string, param2: number): string {
-        return `Param1: ${param1}, Param2: ${param2}`;
+    exampleMethod(param12: string, param2: number): string {
+        return `Param12: ${param12}, Param2: ${param2}`;
     }
 
     /**
