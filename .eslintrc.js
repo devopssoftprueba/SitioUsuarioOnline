bc681d254e68255994b1a3f7581080a3ae27a336
Ronald Pelaez <ronaldpelaez201@gmail.com>
2025-06-04 15:46:56 -0500

    modifico ruta de reglas 3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,23 @@
-module.exports = {
-    "parser": "@typescript-eslint/parser",
-    "plugins": [
-        "@typescript-eslint",
-        "eslint-plugin-tsdoc"
-    ],
-    "extends": [
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "rules": {
-        "tsdoc/syntax": "warn"
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import tsdocPlugin from 'eslint-plugin-tsdoc';
+
+export default [
+    {
+        files: ["**/*.ts", "**/*.tsx"],
+        languageOptions: {
+            parser: typescriptParser,
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: "module"
+            }
+        },
+        plugins: {
+            "@typescript-eslint": typescript,
+            "tsdoc": tsdocPlugin
+        },
+        rules: {
+            "tsdoc/syntax": "warn"
+        }
     }
-};
+];
